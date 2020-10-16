28a704d5fb343e79609f1ca5dbed24f89cdc3f9f
Anendaoctavia <69908254+Anendaoctavia@users.noreply.github.com>
2020-10-16 10:39:36 +0700

    Add files via upload

---
+++ BIODATA.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BIODATA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama="ANENDA OCTAVIA SALSA BILLA";
+        String nim="123456789";
+        int semester=7;
+        String ttl="pasuruan,18 oktober 2003";
+        String alamat="Gerbo";
+        String hobi="bersepeda";
+        String Mfav="bakso";
+        
+        System.out.println("My BIODATA:");
+        System.out.println("---------------");
+        System.out.println("Nama\t\t\t\t:"+nama);
+        System.out.println("NIM\t\t\t\t:"+nim);
+        System.out.println("semester\t\t\t:"+semester);
+        System.out.println("tempat tanggal lahir\t\t:"+ttl);
+        System.out.println("alamat\t\t\t\t:"+alamat);
+        System.out.println("hobi\t\t\t\t:"+hobi);
+        System.out.println("makanan favorit:\t\t"+Mfav);
+        
+    }
+    
+}
